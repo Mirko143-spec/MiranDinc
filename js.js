89ce2af6df44903ba59939649c237fe4cfc7d31d
mirko143 <miran123125@gmail.com>
2025-11-17 03:42:26 +0100

    modal test with external html file

--- js.js
+++ js.js
@@ -1,14 +1,24 @@
 const aboutModal = document.getElementById("about-modal");
 const aboutButton = document.getElementById("about");
 const closeButton = document.getElementById("close");
+const modalContent = document.getElementById("modal-content");
 
 closeButton.addEventListener("click", () => {
   aboutModal.close();
   aboutButton.style.backgroundImage = "url('/images/folder.png')";
 });
 
-aboutButton.addEventListener("click", () => {
-  aboutModal.showModal();
-  console.log("About button clicked");
+aboutButton.addEventListener("click", async () => {
+  try {
+    const res = await fetch("/pages/test.html");
+
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const data = await res.text();
+    modalContent.innerHTML = data;
+    aboutModal.showModal();
+  } catch (error) {}
   aboutButton.style.backgroundImage = "url('/images/open-folder.png')";
 });
