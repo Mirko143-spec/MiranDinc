5b4dcad01d62fd26648713965309a12890033ffa
mirko143 <miran123125@gmail.com>
2026-02-04 21:10:55 +0100

    Made a login page, added a logout button on to the home button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import Desktop from "./components/Desktop.tsx";
 import LogIn from "./components/LogIn.tsx";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
+  function handleLogin() {
+    setLoggedIn(true);
+  }
+
+  function handleLogout() {
+    setLoggedIn(false);
+  }
   return (
     <>
-      <LogIn />
+      {loggedIn ? (
+        <Desktop handleLogout={handleLogout} />
+      ) : (
+        <LogIn handleLogin={handleLogin} />
+      )}
     </>
   );
 }
--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,5 +1,47 @@
-function LogIn() {
-  return <h1>Hello</h1>;
+import { useState } from "react";
+import profile from "../assets/profile.jpeg";
+
+interface LogInProps {
+  handleLogin: () => void;
+}
+
+function LogIn({ handleLogin }: LogInProps) {
+  const [loading, setLoading] = useState<boolean>(false);
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    setLoading(true);
+
+    setTimeout(() => {
+      handleLogin();
+      setLoading(false);
+    }, 1000);
+  }
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="h-72 w-[36rem] bg-transparent flex flex-col justify-center items-center rounded-2xl text-gray-100 p-4">
+        <img src={profile} alt="Profile picture" className="w-36 rounded-2xl" />
+        <h1 className="text-2xl text-slate-100 mb-8 mt-4">Miran</h1>
+        <form
+          onSubmit={handleSubmit}
+          className="my-auto flex flex-col items-center justify-center"
+        >
+          {loading ? (
+            "Loading..."
+          ) : (
+            <div className="flex flex-col items-center">
+              <input
+                placeholder="Password.."
+                className="p-4 outline-none focus:placeholder-transparent rounded-2xl h-12 mx-4 text-slate-400"
+                type="password"
+                autoFocus
+              />
+              <p className="text-sm opacity-75">Hint: any password works</p>
+            </div>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default LogIn;
